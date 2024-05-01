d1c18e8bffd35beac7359f3d226ffda3f08909b0
nadina <solernadina@gmail.com>
2024-05-01 14:36:05 -0300

    funcones

--- template-alumnos/template-alumnos/funciones.py
+++ template-alumnos/template-alumnos/funciones.py
@@ -1,6 +1,10 @@
 import numpy as np
 import networkx as nx
+import scipy
 
+# =============================================================================
+# FUNCION PARA LEER EL ARCHIVO
+# =============================================================================
 
 def leer_archivo(input_file_path):
 
@@ -16,6 +20,10 @@ def leer_archivo(input_file_path):
     f.close()
     
     return W
+
+# =============================================================================
+# FUNCIONES PARA DIBUJAR GRAFO
+# =============================================================================
 
 def dibujarGrafo(W, print_ejes=True):
     
@@ -37,12 +45,101 @@ def dibujarGrafo(W, print_ejes=True):
     
     nx.draw(G, pos=nx.spring_layout(G), **options)
 
+# =============================================================================
+# FUNCIONES AUXILIARES AL CALCULO DEL RANKING
+# =============================================================================
+
+def calculo_D(A):
+    m=A.shape[0] #filas
+    n=A.shape[1] #columnas
+    D = np.zeros((m,n))
+    for i in range(n): #columnas
+        grado = 0
+        for j in range(m): #filas
+            grado += A[j][i]
+        if grado != 0:
+            D[i][i] = 1/grado
+    return D
+
+def calculo_A(W,p):
+    D = calculo_D(W)
+    m = W.shape[0] #filas
+    I = np.diag(np.ones(m),0)
+    A = I - p*W@D
+    return A
+
+def elim_gaussiana(A):
+    cant_op = 0
+    m=A.shape[0] #filas
+    n=A.shape[1] #columnas
+    Ac= A.copy() 
+    Ad =  A.copy() 
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return 
+    
+    ## desde aqui -- CODIGO A COMPLETAR
+    
+    ## Aca calculo los valores de los multiplicadores y lo actualizo en A
+    for j in range (n): #columnas
+        pivote = Ad[j,j]
+        for i in range(1, m): #filas
+            if j < i:
+                k = calculo_k(Ad[i], pivote, j) #calculo k
+                if k != 0:
+                    Ad[i] = Ad[i] - k*Ad[j]
+                    Ac[i][j] = k #agrego k 
+                    cant_op += 1
+                else:
+                    continue
+                
+    ## Aca actualizo los valores arriba de la diagonal
+    for j in range (n): #columnas
+        for i in range(1, m): #filas
+            if j >= i:
+                Ac[i][j] = Ad[i][j]
+    
+    ## hasta aqui
+
+      
+    L = np.tril(Ac,-1) + np.eye(A.shape[0]) 
+    U = np.triu(Ac)
+    return L, U, cant_op
+    
+
+def calculo_k(fila_actual, divisor, iterador):
+    multiplicador = 0
+    if divisor != 0:
+        multiplicador = fila_actual[iterador] / divisor
+    return multiplicador
+   
+def ranking(score):
+    sorted_score = sorted(scr)
+    rnk = [sorted_score.index(x) + 1 for x in scr]
+    res = []
+    for elemento in rnk:
+        if elemento in res:
+            res.append(elemento + 1)
+            
+# =============================================================================
+# FUNCIONES PRINCIPALES PARA EL CALCULO DE RANKING
+# =============================================================================
+
 def calcularRanking(M, p):
     npages = M.shape[0]
     rnk = np.arange(0, npages) # ind{k] = i, la pagina k tienen el iesimo orden en la lista.
     scr = np.zeros(npages) # scr[k] = alpha, la pagina k tiene un score de alpha 
     # COIDGO
-    
+    A = calculo_A(M,p)
+    n = A.shape[1]
+    e = np.ones(n)
+    L, U, cant_op = elim_gaussiana(A)
+    y = scipy.linalg.solve_triangular(L,e, lower=True)
+    x = scipy.linalg.solve_triangular(U,y)
+    norma = scipy.linalg.norm(x,1)
+    scr = x/norma
+    rnk = [sorted_score.index(x) + 1 for x in scr]
     #
     return rnk, scr
 
@@ -53,4 +150,29 @@ def obtenerMaximoRankingScore(M, p):
     output = np.max(scr)
     
     return output
+
+#ARCHIVOS DE ENTRADA
+archivo_test = './tests/test_dosestrellas.txt'
+    
+#CARGA DE ARCHIVO EN GRAFO
+W = leer_archivo(archivo_test)
+
+dibujarGrafo(W, print_ejes=False)
+# defino la probabilidad de salto de continuar los links de la pagina actual
+p = 0.5
+# Realizo el test unitario para el calculo del mayor score, que pruebe que el codigo funciona correctamente.
+print('*'*50)
+print('Test unitario 1')
+try:
+    compara = obtenerMaximoRankingScore(W, p)
+    assert(np.isclose(compara, 0.1811, atol= 0.0001))
+except:
+    print('OUCH!! - No paso el test unitario')
+else:
+    print('BIEN! - Paso correctamente el test unitario')
+print('*'*50)
+
+rnk, scr = calcularRanking(W, p)
+print(rnk)
+print(scr)
 
