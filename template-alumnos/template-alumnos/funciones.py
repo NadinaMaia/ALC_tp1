83d11a6c4626c127c8ecc1476036ffa94ef45162
LNBiasoni <talibiasoni01@gmail.com>
2024-05-02 01:40:38 -0300

    .

--- template-alumnos/template-alumnos/funciones.py
+++ template-alumnos/template-alumnos/funciones.py
@@ -1,7 +1,8 @@
 import numpy as np
 import networkx as nx
 import scipy
-
+import matplotlib.pyplot as plt
+import time
 # =============================================================================
 # FUNCION PARA LEER EL ARCHIVO
 # =============================================================================
@@ -176,3 +177,139 @@ rnk, scr = calcularRanking(W, p)
 print(rnk)
 print(scr)
 
+# =============================================================================
+# FUNCIONES PRINCIPALES PARA ANALISIS CUALITATIVO
+# =============================================================================
+
+def rankings_segunP(M, n):
+    P = [] #guardo los distintos P
+    mejores_paginas = [] #guardo las paginas mejores ranqueadas
+    for i in range(1, n+1):
+        p = 1 / i
+        P.append(p)
+        mejor_pagina = [] #guardo las paginas mejores rankeadas con un P por si hay empate
+        ranking, scores = calcularRanking(M, p)
+        maxScore = np.max(scores)
+        
+        for j in range(len(scores)):
+            score = scores[j]
+            if score == maxScore:
+                m = j 
+                mejor_pagina.append(m)
+                
+        mejores_paginas.append(mejor_pagina)
+
+    return P, mejores_paginas
+
+    
+def Graf_MejoresPaginas_segunP (M, n): 
+    d = M.shape[0]
+    paginas = list(range(d))
+    # Datos
+    Altura = []
+    P, mejores_paginas = rankings_segunP(M, n)
+    for pagina in paginas:
+        contador = 0
+        for m in mejores_paginas:
+            if pagina in m:
+                contador += 1
+        Altura.append(contador)
+    print(Altura)
+    paleta = "Pastel1"
+    # Crear el gráfico de barras horizontales
+    plt.barh(paginas, Altura, color=plt.cm.get_cmap(paleta)(range(len(paginas))), edgecolor='black')
+    # Agregar etiquetas y título
+    plt.xlabel('Cantidad de veces que obtuvieron el mejor Score')
+    plt.ylabel('Páginas')
+    plt.title('Cantidad de veces que una página obtuvo el mejor score variando el p')
+    # Establecer límites de los ejes
+    plt.xlim(0, max(Altura) + 1)  
+    plt.ylim(-0.5, d - 0.5) 
+    # Mostrar el gráfico
+    plt.show()
+  
+
+    
+def graf_rankingP2(M, n):  
+    # Graficar quién fue la página mejor rankeada para cada p
+    P, mejores_paginas = rankings_segunP(M, n)
+    
+    # Inicializar listas para almacenar datos porsi hay empates 
+    y1 = []
+    x = []
+    # Recorrer los valores de p y las páginas mejor rankeadas
+    for m, paginas in enumerate(mejores_paginas):
+        for pagina in paginas:
+            y1.append(pagina)
+            x.append(P[m])
+    
+    plt.scatter(x, y1, s=100, c=x, cmap='spring')
+    
+    plt.xlabel('P')
+    plt.ylabel('Página Mejor Rankeada')
+    plt.title('Página Mejor Rankeada según el P utilizado')
+    plt.colorbar(label='X')
+    plt.show()  
+              
+    #grafico que me muestre el porcentaje de veces que una pagina fue la mejor rankeada variando el P
+def ranking_P3(M, n):   
+    P, mejores_paginas = rankings_segunP(M, n)
+    paginas = list(range(0, M.shape[0])) 
+    porcentajes=[]
+    p=[]
+    for pagina in paginas:
+        contador = 0
+        for m in mejores_paginas:
+            if pagina in m: 
+                contador += 1
+        porcentaje = contador / len(mejores_paginas) * 100
+        if porcentaje != 0:  # Excluir porcentajes de cero
+            porcentajes.append(porcentaje)
+            p.append(pagina)
+    paleta = "Pastel1"
+    # Crear el gráfico de torta
+    plt.pie(porcentajes, labels=p, colors=plt.cm.get_cmap(paleta)(range(len(porcentajes))), autopct='%1.1f%%', wedgeprops={'linewidth': 2})
+    plt.title('Porcentaje de veces que una página fue mejor rankeada') 
+    plt.show()
+
+def medir_tiempo(f, M, n):
+    inicio = time.time()
+    resultado = f (M,n)
+    fin = time.time()
+    tiempo_transcurrido = fin - inicio
+    print(f"Tiempo de ejecución de {f.__name__}: {tiempo_transcurrido} segundos")
+    return resultado
+
+# =============================================================================
+# FUNCIONES PRINCIPALES PARA ANALISIS CUANTITATIVO
+# =============================================================================
+def tiempo_de_ejecucion(f, W, p):
+    inicio = time.time()
+    resultado = f(W,p)
+    fin = time.time()
+    tiempo_transcurrido = fin - inicio
+    return tiempo_transcurrido
+
+def tiempo_de_ejecucion_tamaño (n,p):
+    i=2
+    tiempos=[]
+    tamaños=[]
+    while i <=n:
+        W= np.random.choice([0, 1], size=(i,i))
+        tiempo= tiempo_de_ejecucion(calcularRanking, W, p)
+        tiempos.append(tiempo)
+        tamaños.append(i)
+        i+=1
+    return tiempos, tamaños
+
+def tiempo_ejecucion_densidad (n,p):
+    W=  np.zeros((n, n))
+    tiempo= tiempo_de_ejecucion(calcularRanking, W, p)
+    tiempos= []
+    tiempos.append(tiempo)
+    for i in range (0,n):
+        for j in range (0,n):
+            W[i][j]=1
+            tiempo= tiempo_de_ejecucion(calcularRanking, W, p)
+            tiempos.append(tiempo)
+    return tiempos
