c1c1f6274b4c20eedf8270e303f9eef6d5e67f16
LNBiasoni <talibiasoni01@gmail.com>
2024-05-08 14:59:23 -0300

    .

--- template-alumnos/template-alumnos/funciones.py
+++ template-alumnos/template-alumnos/funciones.py
@@ -36,11 +36,13 @@ def leer_archivo(input_file_path):
 # =============================================================================
 # IMPORTAMOS LOS ARCHIVOS
 # =============================================================================
-I= leer_archivo("/test/" + "instagram_famosos_grafo.txt") 
-M= leer_archivo("/test/"  + "mathworld_grafo.txt") 
-DS= leer_archivo("/test/"  + "test_dosestrellas.txt") 
-A= leer_archivo("/test/" + "test_aleatorio.txt")
-TS= leer_archivo("/test/"  + "test_30_segundos.txt")
+carpeta = "/home/oem/Descargas/template-alumnos/template-alumnos/tests"
+I= leer_archivo(carpeta + "instagram_famosos_grafo.txt") 
+M= leer_archivo(carpeta + "mathworld_grafo.txt") 
+DS= leer_archivo(carpeta + "test_dosestrellas.txt") 
+A= leer_archivo(carpeta+ "test_aleatorio.txt")
+TS= leer_archivo(carpeta + "test_30_segundos.txt")
+
 # =============================================================================
 # FUNCIONES PARA DIBUJAR GRAFO
 # =============================================================================
